e21e1dfb909e1b9fc579d234f599bf46460e540d
ManuelPC20 <48839992+ManuelPC20@users.noreply.github.com>
2019-03-27 19:16:13 -0500

    qr reader

---
+++ QR Project/Assets/APP/Scripts/QrCodeReader.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using ZXing;
+using ZXing.QrCode;
+using UnityEngine;
+using System;
+
+
+public class QrCodeReader : MonoBehaviour {
+
+    private WebCamTexture camTexture;
+    private Rect screenRect;
+    public bool detecto;
+    public AudioSource Audio;
+
+    void Start()
+    {
+        screenRect = new Rect(0, 0, Screen.width, Screen.height);
+        camTexture = new WebCamTexture();
+        camTexture.requestedHeight = Screen.height;
+        camTexture.requestedWidth = Screen.width;
+        if (camTexture != null)
+        {
+            camTexture.Play();
+        }
+    }
+
+    void OnGUI()
+    {
+
+        
+        GUIStyle letrastyle = new GUIStyle();
+        letrastyle.fontSize = 50; 
+        // drawing the camera on screen
+        GUI.DrawTexture(screenRect, camTexture, ScaleMode.ScaleToFit);
+        // do the reading — you might want to attempt to read less often than you draw on the screen for performance sake
+        try
+           {
+            
+            IBarcodeReader barcodeReader = new BarcodeReader();
+            // decode the current frame
+            var result = barcodeReader.Decode(camTexture.GetPixels32(),
+              camTexture.width, camTexture.height);
+
+            if (result != null && detecto == false)
+            {
+                detecto = true;
+                GUI.TextField(new Rect(150, 650, 500, 50), result.Text, letrastyle);
+               Audio.Play();
+            }
+            else if
+            (result!=null && detecto == true)
+            {
+                detecto = true;
+                GUI.TextField(new Rect(150, 650, 500, 50), result.Text, letrastyle);
+            }
+
+            if(result== null)
+            {
+                detecto = false;
+            }
+            
+            }
+         catch (Exception ex)
+        {
+            detecto = false;
+            Debug.LogWarning(ex.Message);         
+        }
+
+     
+    }
+
+}
---
+++ QR Project/Assets/APP/Scripts/QrCodeReader2.cs
@@ -0,0 +1,186 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using ZXing;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using LitJson;
+using System.IO;
+using Crosstales.RTVoice;
+
+public class QrCodeReader2 : MonoBehaviour
+{
+
+    public bool mujer;
+    private Rect screenRect;
+    public DatosInvitados datosinvitados;
+    private WebCamTexture camTexture;
+    public bool activado;
+    private string resultado;
+    private string oldResultado;
+    public string[] splited;
+    public string[] splitedfinal;
+    public string[] splitnombre;
+    private AudioSource audiocamera;
+
+
+    private Animator imagen1;
+
+    public List<string> detectardni = new List<string>();
+    public bool detected;
+    public AudioSource Audio;
+    public Text mostrarResultado;
+    public GameObject canvasTexto;
+    JsonData dataJson;
+
+
+
+
+    void Start()
+    {
+        imagen1 = GameObject.Find("Imagen1").GetComponent<Animator>();
+        audiocamera = GameObject.Find("Main Camera").GetComponent<AudioSource>();
+        camTexture = new WebCamTexture();
+        camTexture.requestedHeight = Screen.height;
+        camTexture.requestedWidth = Screen.width;
+        if (camTexture != null)
+        {
+            camTexture.Play();
+        }
+    }
+
+    private void Update()
+    {
+        Reaccion();
+    }
+
+    public void Reaccion()
+    {
+        IBarcodeReader barcodeReader = new BarcodeReader();
+        var result = barcodeReader.Decode(camTexture.GetPixels32(),
+        camTexture.width, camTexture.height);
+        resultado = Convert.ToString(result);
+
+        if (result != null)
+        {
+            if (oldResultado == resultado)
+            {
+                if (activado == false)
+                {
+                    StartCoroutine(RepetirImagen());
+                    activado = true;
+                    return;
+                }
+
+            }
+            else
+            {
+
+                StartCoroutine(CambiarImagen());
+                Debug.Log(resultado);
+                oldResultado = resultado;
+                canvasTexto.SetActive(true);
+                splited = resultado.Split(':', '\n', '\r');
+
+
+                datosinvitados = new DatosInvitados(splited[1], splited[4], splited[7], splited[10], splited[13]);
+
+                splitnombre = splited[1].Split(' ', '\r');
+
+
+                dataJson = JsonMapper.ToJson(datosinvitados);
+
+
+                File.AppendAllText(Application.dataPath + "/Resources/datosclientes.json", dataJson.ToString() + ",", Encoding.UTF8);
+                StartCoroutine(SpeakerON());
+
+            }
+
+        }
+
+    }
+
+
+    public IEnumerator SpeakerON()
+    {
+
+        yield return new WaitForSeconds(1.0f);
+
+
+        if (!audiocamera.isPlaying && mujer == false)
+        {
+            mostrarResultado.text = "Dr." + " " + splitnombre[1];
+            Speaker.Speak("Bienvenido Doctor" + splitnombre[1], audiocamera, Speaker.VoiceForName("Microsoft Helena Desktop"), true, 0.8f, 1);
+        }
+        else if (!audiocamera.isPlaying && mujer == true)
+        {
+            mostrarResultado.text = "Dra." + " " + splitnombre[1];
+            Speaker.Speak("Bienvenida Doctora" + splitnombre[1], audiocamera, Speaker.VoiceForName("Microsoft Helena Desktop"), true, 0.8f, 1);
+            mujer = false;
+        }
+
+
+    }
+
+
+    public void MujerBoton()
+    {
+        if (mujer == false)
+        {
+            mujer = true;
+        }
+    }
+
+    public IEnumerator CambiarImagen()
+    {
+        imagen1.SetBool("Cambiar", true);
+        yield return new WaitForSeconds(4.0f);
+        mostrarResultado.text = "";
+        imagen1.SetBool("Cambiar", false);
+
+    }
+
+    public IEnumerator RepetirImagen()
+    {
+        mostrarResultado.text = "Dr." + " " + splitnombre[1];
+        if (!audiocamera.isPlaying && mujer == false)
+        {
+            Speaker.Speak("Bienvenido Doctor" + splitnombre[1], audiocamera, Speaker.VoiceForName("Microsoft Helena Desktop"), true, 0.8f, 1);
+        }
+        else if (!audiocamera.isPlaying && mujer == true)
+        {
+            mostrarResultado.text = "Dra." + " " + splitnombre[1];
+            Speaker.Speak("Bienvenida Doctora" + splitnombre[1], audiocamera, Speaker.VoiceForName("Microsoft Helena Desktop"), true, 0.8f, 1);
+            mujer = false;
+        }
+        imagen1.SetBool("Cambiar", true);
+        yield return new WaitForSeconds(4.0f);
+        imagen1.SetBool("Cambiar", false);
+        mostrarResultado.text = "";
+        activado = false;
+
+    }
+
+}
+
+public class DatosInvitados
+{
+    public string NombresApellidos;
+    public string Dni;
+    public string ColegioMedico;
+    public string Correo;
+    public string Celular;
+
+    public DatosInvitados(string NombresApellidos, string Dni, string ColegioMedico, string Correo, string Celular)
+    {
+
+        this.NombresApellidos = NombresApellidos;
+        this.Dni = Dni;
+        this.ColegioMedico = ColegioMedico;
+        this.Correo = Correo;
+        this.Celular = Celular;
+
+    }
+
+}
